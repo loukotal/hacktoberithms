025defeed0388ad9adb4e549c7dc4f6154d583b2
Lukáš Loukota <lukasloukota94@gmail.com>
2018-10-07 00:21:39 +0200

    Handle repeated characters

--- intermediate/mutations_loukotal.py
+++ intermediate/mutations_loukotal.py
@@ -3,11 +3,14 @@ Write a function that satisfies the following rules:
 
 Return true if the string in the first element of the list contains all of the letters of the string in the second element of the list.
 """
+from collections import Counter
 
 def mutation(input_list):
     short, long = sorted(map(lambda l: l.lower(), input_list), key=lambda item: len(item))
+    counter_short = Counter(short)
+    counter_long = Counter(long)
     for letter in short:
-        if letter not in long:
+        if letter not in long or counter_short[letter] != counter_long[letter]:
             return False
     return True
 
